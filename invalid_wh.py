504de6b05e637f9e8085dfe04feb00a5329ecd81
suryathanush <64359300+suryathanush@users.noreply.github.com>
2021-02-01 12:19:39 +0530

    Update invalid_wh.py
    check if the number has whatsapp or not, if not then prints phone number invalid

--- invalid_wh.py
+++ invalid_wh.py
@@ -6,6 +6,7 @@ options.add_argument("user-data-dir=Home/bot/.config/chromium/Profile 1")
 
 driver = webdriver.Chrome(executable_path='/home/suryathanush/bot/chromedriver',options=options)
 driver.get("https://web.whatsapp.com/send?phone=<invalid_number>&text=hi")
+
 while(1):
     try:
         user = driver.find_element_by_class_name('_2fuxX').text
