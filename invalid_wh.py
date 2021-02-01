04e79b0ec54c8adce9b41d23a9ec0b6d3bc7ee2c
suryathanush <64359300+suryathanush@users.noreply.github.com>
2021-02-01 14:28:22 +0530

    Update invalid_wh.py

--- invalid_wh.py
+++ invalid_wh.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=Home/bot/.config/chromium/Profile 1")
+options.add_argument("user-data-dir=Home/bot/.config/chromium/Profile 1") //chrome configuration file path for user
 
-driver = webdriver.Chrome(executable_path='/home/suryathanush/bot/chromedriver',options=options)
+driver = webdriver.Chrome(executable_path='<chrome driver file path>',options=options)
 driver.get("https://web.whatsapp.com/send?phone=<invalid_number>&text=hi")
 
 while(1):
